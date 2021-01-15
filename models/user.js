4d6f51eb5d5a1c0310d93bc0429d2682e905cec1
royal0721 <royal0721@gmail.com>
2021-01-15 20:02:06 +0800

    added log in

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ const UserSchema = new mongoose.Schema({
     hash: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    createdDate: { type: Date, default: Date.now }
+    createdDate: { type: Date, default: Date.now },
+    role:{type:Number,required:true}
 });
 
 UserSchema.set('toJSON', {
--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,7 @@ function authenticate(req, res, next) {
 
 function register(req, res, next) {
     userService.create(req.body)
-        .then(() => res.json({}))
+        .then(() => res.json({message: 'OK'}))
         .catch(err => next(err));
 }
 
